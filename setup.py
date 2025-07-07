cbfa44087859c8c8481855b169668b06ed70c15f
mizitm001 <rumorgirl.season18@gmail.com>
2025-07-07 09:30:57 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DivaDivaModule",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        'Pillow',
+        'tkinter',
+    ],
+    entry_points={
+        'console_scripts': [
+            'divadivamodule = divadivamodule:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['images/*', 'divadivamodule.py', 'module_data.csv'],
+    },
+)
